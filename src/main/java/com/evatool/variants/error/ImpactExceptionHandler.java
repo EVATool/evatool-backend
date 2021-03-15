f03285ae6ba3c34e53149f41b5453c0526b4721b
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2021-03-15 16:05:26 +0100

    adding Exception Handler

---
+++ src/main/java/com/evatool/variants/error/ImpactExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.evatool.variants.error;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class ImpactExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ImpactExceptionHandler.class);
+
+
+
+
+
+    private String getUri(WebRequest webRequest) {
+        return ((ServletWebRequest) webRequest).getRequest().getRequestURI();
+    }
+}
