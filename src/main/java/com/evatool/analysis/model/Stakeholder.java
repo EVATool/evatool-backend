c5639c63a551063b571eb3a2c3c573532f12db5e
Henning Buhl <henning.buhl@yahoo.de>
2021-03-15 22:24:04 +0100

    Fixed stakeholder receive events in impact and tests

--- src/main/java/com/evatool/analysis/model/Stakeholder.java
+++ src/main/java/com/evatool/analysis/model/Stakeholder.java
@@ -1,6 +1,7 @@
 package com.evatool.analysis.model;
 
 import com.evatool.analysis.enums.StakeholderLevel;
+import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -73,5 +74,10 @@ public class Stakeholder {
             throw new IllegalArgumentException("priority name cannot be null.");
         }
         this.priority = priority;
+    }
+
+    public String toJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 }
