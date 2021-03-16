9849f6e260c8973d3655e8965c97c1d2073cb565
Henning Buhl <henning.buhl@yahoo.de>
2021-03-16 22:30:53 +0100

    Changed CORS config

--- src/main/java/com/evatool/global/config/WebConfig.java
+++ src/main/java/com/evatool/global/config/WebConfig.java
@@ -1,20 +1,16 @@
 package com.evatool.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
