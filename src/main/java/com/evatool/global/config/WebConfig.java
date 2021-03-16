0ca00505fdf8960cc5b7915cd5a8a6348b3396a2
Henning Buhl <henning.buhl@yahoo.de>
2021-03-16 22:55:36 +0100

    Readded CORS config

--- src/main/java/com/evatool/global/config/WebConfig.java
+++ src/main/java/com/evatool/global/config/WebConfig.java
@@ -5,4 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
