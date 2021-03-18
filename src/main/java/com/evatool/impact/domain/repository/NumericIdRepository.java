0ddef0b7b4a44fdd96cf5c3e10889293c2525c49
Henning Buhl <henning.buhl@yahoo.de>
2021-03-18 19:29:07 +0100

    Removed NumericIdRepository

--- src/main/java/com/evatool/impact/domain/repository/NumericIdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.evatool.impact.domain.repository;
-
-import com.evatool.impact.domain.entity.NumericImpactId;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NumericIdRepository extends CrudRepository<NumericImpactId, Integer> {
-}
