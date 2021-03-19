dfc79bb2f86218583a53a38f237eb12cb22e6639
Henning Buhl <henning.buhl@yahoo.de>
2021-03-19 13:36:15 +0100

    Create NumericIdTest.java

---
+++ src/test/java/com/evatool/impact/domain/entity/NumericIdTest.java
@@ -0,0 +1,44 @@
+package com.evatool.impact.domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static com.evatool.impact.common.TestDataGenerator.createDummyNumericId;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class NumericIdTest {
+
+    @Test
+    void testCreate_CreatedNumericId_NumericIdIsNull() {
+        assertThat(createDummyNumericId().getNumericId()).isNull();
+    }
+
+    @Test
+    void testCreate_CreatedNumericId_ReadableIdIsNull() {
+        assertThat(createDummyNumericId()._getReadableId()).isNull();
+    }
+
+    @Test
+    void testGetReadableId_ExistingId_ReturnReadableId() {
+        // given
+        var numericId = createDummyNumericId();
+
+        // when
+        numericId.setNumericId(1);
+
+        // then
+        assertThat(numericId._getReadableId()).isEqualTo("IMP" + numericId.getNumericId());
+    }
+
+    @Test
+    void testSetNumericId_ExistingId_ThrowNewIllegalArgumentException() {
+        // given
+        var numericId = createDummyNumericId();
+
+        // when
+        numericId.setNumericId(1);
+
+        // then
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> numericId.setNumericId(2));
+    }
+}
