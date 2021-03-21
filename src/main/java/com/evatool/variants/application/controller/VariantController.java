0b56314b26001a0447eb8de4d3cbe150ea183191
mto3737 <64856860+mto3737@users.noreply.github.com>
2021-03-21 12:13:46 +0100

    implement getVariantsByAnalysis add logging
    implement getVariantsByAnalysis
    add logging

--- src/main/java/com/evatool/variants/application/controller/VariantController.java
+++ src/main/java/com/evatool/variants/application/controller/VariantController.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,7 @@ import static com.evatool.variants.application.services.VariantsUriHelper.VARIAN
 @RestController
 public class VariantController {
 
+    final Logger logger = LoggerFactory.getLogger(VariantController.class);
 
     @Autowired
     VariantService variantService;
@@ -30,7 +33,7 @@ public class VariantController {
     @ApiResponse(code = 201, message = "Created successfully", response = VariantDto.class, responseContainer = "Variant")
     @PostMapping(VARIANTS)
     public ResponseEntity<VariantDto> createVariant(@RequestBody VariantDto newVariantDto) {
-
+        logger.info("[POST] /variants");
         return new ResponseEntity<>(variantService.createVariant(newVariantDto), HttpStatus.CREATED);
     }
 
@@ -40,15 +43,23 @@ public class VariantController {
     public ResponseEntity<VariantDto> getVariant(
             @ApiParam(name = "variantId", value = "identification of a Variant", required = true)
             @PathVariable UUID id) {
-
+        logger.info("[GET] /variants/{id}");
         return new ResponseEntity<>(variantService.getVariant(id), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "getVariantsByAnalysis", notes = "get list of all variants by analysis id", nickname = "getVariantsByAnalysis")
+    @ApiResponse(code = 201, message = "Created successfully", response = VariantDto.class, responseContainer = "Variant")
+    @GetMapping(value = VARIANTS,params = "analysisId")
+    public ResponseEntity<CollectionModel<VariantDto>> getVariantsByAnalysis(@RequestParam("analysisId") UUID analysisId){
+        logger.info("[GET] /variants?analysisId={id}");
+        return new ResponseEntity<>(variantService.getVariantsByAnalysis(analysisId), HttpStatus.OK);
     }
 
     @ApiOperation(value = "getAllVariants", notes = "get list of all variants", nickname = "getAllVariants")
     @ApiResponse(code = 200, message = "Successful retrieval", response = VariantDto.class, responseContainer = "List")
     @GetMapping(VARIANTS)
     public ResponseEntity<CollectionModel<VariantDto>> getAllVariants() {
-
+        logger.info("[GET] /variants");
         return new ResponseEntity<>(variantService.getAllVariants(), HttpStatus.OK);
     }
 
@@ -57,10 +68,9 @@ public class VariantController {
     @PutMapping(VARIANTS_ID)
     public ResponseEntity<VariantDto> updateVariant(
             @ApiParam(name = "variantId", value = "identification of a Variant", required = true)
-            @PathVariable UUID id,
             @RequestBody VariantDto updatedVariantDto) {
-
-        return new ResponseEntity<>(variantService.updateVariant(id, updatedVariantDto), HttpStatus.OK);
+        logger.info("[PUT] /variants");
+        return new ResponseEntity<>(variantService.updateVariant(updatedVariantDto), HttpStatus.OK);
     }
 
     @ApiOperation(value = "deleteVariants", notes = "delete a variants", nickname = "deleteVariant")
@@ -69,7 +79,7 @@ public class VariantController {
     public ResponseEntity<Void> deleteVariant(
             @ApiParam(name = "variantId", value = "identification of a Variant", required = true)
             @PathVariable UUID id) {
-
+        logger.info("[DELETE] /variants/{id}");
         variantService.deleteVariant(id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/evatool/variants/application/services/VariantService.java
+++ src/main/java/com/evatool/variants/application/services/VariantService.java
@@ -45,7 +45,7 @@ public class VariantService {
     VariantMapper variantMapper;
 
     public List<VariantDto> findAll(List<Variant> variantList){
-        logger.info("Find all Variants ");
+        logger.info("Find all Variants");
         return variantMapper.mapAll(variantList);
     }
 
@@ -56,6 +56,7 @@ public class VariantService {
      * @return VariantDTO
      */
     public VariantDto getVariant(UUID id) {
+        logger.debug("findById [{}]",id);
         Variant variant = variantRepository.findVariantById(id);
         if (variant == null) {
             throw new VariantsEntityNotFoundException(id.toString());
@@ -66,31 +67,48 @@ public class VariantService {
 
 
     /**
+     * Returns all Variants from the Repository by analysis id
+     *
+     * @return Collection of VariantDTO
+     */
+    public CollectionModel<VariantDto> getVariantsByAnalysis(UUID id) {
+        logger.debug("findByAnalysisId [{}]",id);
+        List<Variant> variants = variantRepository.findAll();
+        variants.removeIf(variant -> !variant.getVariantsAnalysis().getAnalysisId().equals(id));
+        variants.forEach(variant -> variant.add(linkTo(VariantController.class).slash(variant.getId()).withSelfRel()));
+        Link variantsLink = linkTo(methodOn(VariantController.class).getAllVariants()).withSelfRel();
+        CollectionModel<VariantDto> variantCollectionModel = CollectionModel.of(variantMapper.mapAll(variants));
+        variantCollectionModel.add(variantsLink);
+        return variantCollectionModel;
+    }
+
+
+    /**
      * Returns all Variants from the Repository
      *
      * @return Collection of VariantDTO
      */
     public CollectionModel<VariantDto> getAllVariants() {
+        logger.info("getAllVariants");
         List<Variant> variants = variantRepository.findAll();
         variants.forEach(variant -> variant.add(linkTo(VariantController.class).slash(variant.getId()).withSelfRel()));
         Link variantsLink = linkTo(methodOn(VariantController.class).getAllVariants()).withSelfRel();
         CollectionModel<VariantDto> variantCollectionModel = CollectionModel.of(variantMapper.mapAll(variants));
         variantCollectionModel.add(variantsLink);
         return variantCollectionModel;
-
     }
 
     /**
      * Updates an existing Variant from the Repository and publishes a corresponding event
      *
-     * @param id             Existing id for updating related Variant
      * @param updatedVariant Overwrites the Variant that will be updated
      * @return VariantDTO
      */
-    public VariantDto updateVariant(UUID id, VariantDto updatedVariant) {
-        Variant variant = variantRepository.findVariantById(id);
+    public VariantDto updateVariant(VariantDto updatedVariant) {
+        logger.debug("update [{}]",updatedVariant);
+        Variant variant = variantRepository.findVariantById(updatedVariant.getId());
         if (variant == null) {
-            throw new VariantsEntityNotFoundException(id.toString());
+            throw new VariantsEntityNotFoundException(updatedVariant.getId().toString());
         } else {
             Variant savedVariant = variantRepository.save(variantMapper.fromDto(updatedVariant));
             variantsEventPublisher.publishEvent(new VariantUpdatedEvent(savedVariant.toJson()));
@@ -104,6 +122,7 @@ public class VariantService {
      * @param id Existing id for deleting related Variant
      */
     public void deleteVariant(UUID id) {
+        logger.debug("delete [{}]",id);
         Variant variant = variantRepository.findVariantById(id);
         if (variant == null) {
             throw new VariantsEntityNotFoundException(id.toString());
@@ -120,6 +139,7 @@ public class VariantService {
      * @return VariantDTO
      */
     public VariantDto createVariant(VariantDto newVariantDto) {
+        logger.debug("create [{}]",newVariantDto);
         Variant newVariant = variantRepository.save(variantMapper.fromDto(newVariantDto));
         variantsEventPublisher.publishEvent(new VariantCreatedEvent(newVariant.toJson()));
         return variantMapper.toDto(newVariant);
