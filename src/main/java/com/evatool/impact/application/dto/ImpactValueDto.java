012a0e10f301d28f40b4a8b776ff06cacfdd65c1
Henning Buhl <henning.buhl@yahoo.de>
2021-03-30 12:04:31 +0200

    Adjusted ValueDto

--- src/main/java/com/evatool/impact/application/dto/ImpactValueDto.java
+++ src/main/java/com/evatool/impact/application/dto/ImpactValueDto.java
@@ -3,14 +3,18 @@ package com.evatool.impact.application.dto;
 import com.evatool.impact.domain.entity.ImpactValue;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 import java.util.UUID;
 
-@ApiModel(value = "ImpactValue", description = "ImpactValue of an Values")
+@ApiModel(value = "ImpactValue", description = "Value of an Impact")
+@EqualsAndHashCode
+@ToString
 public class ImpactValueDto {
 
     @ApiModelProperty
@@ -35,27 +39,4 @@ public class ImpactValueDto {
     @Setter
     @NotNull
     private String description;
-
-    @Override
-    public String toString() {
-        return "ValueDto{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", type='" + impactValue + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ImpactValueDto that = (ImpactValueDto) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && impactValue == that.impactValue && Objects.equals(description, that.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, impactValue, description);
-    }
 }
