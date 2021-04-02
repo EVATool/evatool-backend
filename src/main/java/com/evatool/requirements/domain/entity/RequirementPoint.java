92b03300c5eb6b4250f171af6629ca7ff83f67bc
Henning Buhl <henning.buhl@yahoo.de>
2021-04-02 18:08:44 +0200

    Removed warnings

--- src/main/java/com/evatool/requirements/domain/entity/RequirementPoint.java
+++ src/main/java/com/evatool/requirements/domain/entity/RequirementPoint.java
@@ -2,7 +2,7 @@
 package com.evatool.requirements.domain.entity;
 
 
-import com.evatool.requirements.common.exceptions.IllegalDtoValueExcpetion;
+import com.evatool.requirements.common.exceptions.IllegalDtoValueException;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -46,7 +46,7 @@ public class RequirementPoint {
 
     public void setPoints(Double points) throws IllegalArgumentException {
         if (points < -1 || points > 1) {
-            throw new IllegalDtoValueExcpetion("ImpactValue must be in range [-1, 1]");
+            throw new IllegalDtoValueException("ImpactValue must be in range [-1, 1]");
         }
 
         this.points = points;
