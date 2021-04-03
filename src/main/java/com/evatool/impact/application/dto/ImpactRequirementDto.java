dc1c0003a66eb3dcc2473f5ac6d22c271366efb0
pschm <philipp.schmeier@gmail.com>
2021-04-03 17:10:51 +0200

    add ImpactRequirementDtoMapper and tests

--- src/main/java/com/evatool/impact/application/dto/ImpactRequirementDto.java
+++ src/main/java/com/evatool/impact/application/dto/ImpactRequirementDto.java
@@ -2,13 +2,17 @@ package com.evatool.impact.application.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @ApiModel(value = "ImpactRequirement", description = "Requirement of an impact")
+@EqualsAndHashCode
+@ToString
 public class ImpactRequirementDto {
 
     @ApiModelProperty
--- src/main/java/com/evatool/impact/application/dto/mapper/ImpactRequirementDtoMapper.java
+++ src/main/java/com/evatool/impact/application/dto/mapper/ImpactRequirementDtoMapper.java
@@ -1,5 +1,7 @@
 package com.evatool.impact.application.dto.mapper;
 
+import com.evatool.impact.application.dto.ImpactRequirementDto;
+import com.evatool.impact.domain.entity.ImpactRequirement;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +15,13 @@ public class ImpactRequirementDtoMapper {
 
     private static final ModelMapper modelMapper = new ModelMapper();
 
-//    public static ImpactRequirement fromDto(ImpactRequirementDto impactRequirementDto) {
-//
-//    }
+    public static ImpactRequirement fromDto(ImpactRequirementDto impactRequirementDto) {
+        logger.info("Mapping Dto to Entity");
+        return modelMapper.map(impactRequirementDto, ImpactRequirement.class);
+    }
+
+    public static ImpactRequirementDto toDto(ImpactRequirement impactRequirement) {
+        logger.info("Mapping Entity to Dto");
+        return modelMapper.map(impactRequirement, ImpactRequirementDto.class);
+    }
 }
