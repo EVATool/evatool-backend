1423942fa18dee2d1ff691817ea9e663b861b1d4
John-Bryan Spieker <John-Bryan.Spieker@t-online.de>
2021-04-11 23:03:32 +0200

    rename content

--- src/main/java/com/evatool/requirements/domain/entity/SuperEntityUuidGenerator.java
+++ src/main/java/com/evatool/requirements/domain/entity/SuperEntityUuidGenerator.java
@@ -17,29 +17,29 @@ public class SuperEntityUuidGenerator extends UUIDGenerator {
         logger.debug("Generating new SuperEntityUuid");
 
         if (object.getClass() == Requirement.class) {
-            var impact = (Requirement) object;
+            var requirement = (Requirement) object;
 
-            // Check existing impacts for given analysis.
-            var impactsPerAnalysisQuery = session.createQuery("select a.requirmentsPerAnalysis from req_requirements_per_analysis a where a.analysisId=?1", Integer.class);
-            impactsPerAnalysisQuery.setParameter(1, impact.getRequirementsAnalysis().getAnalysisId().toString());
-            var impactsPerAnalysisResult = impactsPerAnalysisQuery.getResultList();
-            var impactsPerAnalysis = impactsPerAnalysisResult.isEmpty() ? 0 : impactsPerAnalysisResult.get(0);
+            // Check existing requirement for given analysis.
+            var requirementPerAnalysisQuery = session.createQuery("select a.requirmentsPerAnalysis from req_requirements_per_analysis a where a.analysisId=?1", Integer.class);
+            requirementPerAnalysisQuery.setParameter(1, requirement.getRequirementsAnalysis().getAnalysisId().toString());
+            var requirementPerAnalysisResult = requirementPerAnalysisQuery.getResultList();
+            var requirementPerAnalysis = requirementPerAnalysisResult.isEmpty() ? 0 : requirementPerAnalysisResult.get(0);
 
-            if (impactsPerAnalysis == 0) { // First impact for that analysis
-                impactsPerAnalysis = 1;
+            if (requirementPerAnalysis == 0) { // First impact for that analysis
+                requirementPerAnalysis = 1;
                 var insertQuery = session.createNativeQuery("insert into req_requirements_per_analysis (ANALYSIS_ID, REQUIRMENTS_PER_ANALYSIS) values (?1, ?2)");
-                insertQuery.setParameter(1, impact.getRequirementsAnalysis().getAnalysisId().toString());
-                insertQuery.setParameter(2, impactsPerAnalysis);
+                insertQuery.setParameter(1, requirement.getRequirementsAnalysis().getAnalysisId().toString());
+                insertQuery.setParameter(2, requirementPerAnalysis);
                 insertQuery.executeUpdate();
             } else {
-                impactsPerAnalysis += 1;
+                requirementPerAnalysis += 1;
                 var updateQuery = session.createQuery("update req_requirements_per_analysis set REQUIRMENTS_PER_ANALYSIS=?1 where ANALYSIS_ID=?2");
-                updateQuery.setParameter(1, impactsPerAnalysis);
-                updateQuery.setParameter(2, impact.getRequirementsAnalysis().getAnalysisId().toString());
+                updateQuery.setParameter(1, requirementPerAnalysis);
+                updateQuery.setParameter(2, requirement.getRequirementsAnalysis().getAnalysisId().toString());
                 updateQuery.executeUpdate();
             }
 
-            impact.setNumericId(impactsPerAnalysis);
+            requirement.setNumericId(requirementPerAnalysis);
         }
 
         Serializable id = session.getEntityPersister(null, object).getClassMetadata().getIdentifier(object, session);
