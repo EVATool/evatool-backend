f351510717ee7522cb45f6dcc5a925750e7e8cb2
Henning Buhl <henning.buhl@yahoo.de>
2021-05-30 23:12:06 +0200

    Create PrefixIdMapperTest.java

---
+++ src/test/java/com/evatool/application/mapper/PrefixIdMapperTest.java
@@ -0,0 +1,28 @@
+package com.evatool.application.mapper;
+
+import com.evatool.application.dto.AnalysisDto;
+import com.evatool.common.exception.PropertyIsInvalidException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@SpringBootTest
+class PrefixIdMapperTest {
+
+    @Autowired
+    private AnalysisMapper mapper;
+
+    @Test
+    void testAmendFromDto_PrefixIdInvalid_Throw() {
+        // given
+        var dto = new AnalysisDto("", "", false, null, null);
+
+        // when
+        dto.setPrefixSequenceId("Invalid");
+
+        // then
+        assertThatExceptionOfType(PropertyIsInvalidException.class).isThrownBy(() -> mapper.fromDto(dto));
+    }
+}
