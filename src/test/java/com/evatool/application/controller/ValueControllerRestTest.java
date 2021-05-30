d87c8e67e8f2f843e4a6eddd95f49a852694eb4a
Henning Buhl <henning.buhl@yahoo.de>
2021-05-30 22:19:40 +0200

    Added ValueType test

--- src/test/java/com/evatool/application/controller/ValueControllerRestTest.java
+++ src/test/java/com/evatool/application/controller/ValueControllerRestTest.java
@@ -1,8 +1,27 @@
 package com.evatool.application.controller;
 
 import com.evatool.application.dto.ValueDto;
+import com.evatool.common.enums.ValueType;
 import com.evatool.domain.entity.Value;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
-public class ValueControllerRestTest extends CrudControllerRestTest<Value, ValueDto> implements FindByAnalysisControllerRestTest {
+import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValueControllerRestTest extends CrudControllerRestTest<Value, ValueDto> implements FindByAnalysisControllerRestTest {
+
+    @Test
+    void testGetValueTypes() {
+        // given
+
+        // when
+        var valueTypes = rest.getForEntity(UriUtil.VALUES_TYPES, ValueType[].class);
+
+        // then
+        assertThat(valueTypes.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(valueTypes.getBody()).hasSize(ValueType.values().length);
+        assertThat(valueTypes.getBody()).isEqualTo(ValueType.values());
+    }
 }
