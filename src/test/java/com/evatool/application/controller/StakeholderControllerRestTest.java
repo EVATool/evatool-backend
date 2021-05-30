ef1c3c18ac0dd1e9a4f9ff149421a9ce44b88b27
Henning Buhl <henning.buhl@yahoo.de>
2021-05-30 23:14:32 +0200

    Added stakeholder level and priority tests

--- src/test/java/com/evatool/application/controller/StakeholderControllerRestTest.java
+++ src/test/java/com/evatool/application/controller/StakeholderControllerRestTest.java
@@ -1,8 +1,39 @@
 package com.evatool.application.controller;
 
 import com.evatool.application.dto.StakeholderDto;
+import com.evatool.common.enums.StakeholderLevel;
+import com.evatool.common.enums.StakeholderPriority;
 import com.evatool.domain.entity.Stakeholder;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
-public class StakeholderControllerRestTest extends CrudControllerRestTest<Stakeholder, StakeholderDto> implements FindByAnalysisControllerRestTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class StakeholderControllerRestTest extends CrudControllerRestTest<Stakeholder, StakeholderDto> implements FindByAnalysisControllerRestTest {
+
+    @Test
+    void testGetStakeholderLevels() {
+        // given
+
+        // when
+        var stakeholderLevels = rest.getForEntity(UriUtil.STAKEHOLDERS_LEVELS, StakeholderLevel[].class);
+
+        // then
+        assertThat(stakeholderLevels.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(stakeholderLevels.getBody()).hasSize(StakeholderLevel.values().length);
+        assertThat(stakeholderLevels.getBody()).isEqualTo(StakeholderLevel.values());
+    }
+
+    @Test
+    void testGetStakeholderPriorities() {
+        // given
+
+        // when
+        var stakeholderPriorities = rest.getForEntity(UriUtil.STAKEHOLDERS_PRIORITIES, StakeholderPriority[].class);
+
+        // then
+        assertThat(stakeholderPriorities.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(stakeholderPriorities.getBody()).hasSize(StakeholderPriority.values().length);
+        assertThat(stakeholderPriorities.getBody()).isEqualTo(StakeholderPriority.values());
+    }
 }
