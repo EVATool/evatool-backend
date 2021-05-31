af12febadcb46d43fe258500ae27134b17d8328f
Henning Buhl <henning.buhl@yahoo.de>
2021-05-31 11:45:51 +0200

    Added TODO

--- src/test/java/com/evatool/common/exception/EntityNotFoundExceptionTest.java
+++ src/test/java/com/evatool/common/exception/EntityNotFoundExceptionTest.java
@@ -5,17 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-class EntityNotFoundExceptionTest {
-
-    @Test
-    void testEntityNotFoundException() {
-        // given
-        var exception = new EntityNotFoundException(Analysis.class.getSimpleName(), UUID.randomUUID());
-
-        // when
+class EntityNotFoundExceptionTest { // TODO should exceptions be tested?
 
 
-        // then
-
-    }
 }
