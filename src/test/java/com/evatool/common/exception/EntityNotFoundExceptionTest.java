f465058838c8629d5d0ddc7aca63c22a8dd48a07
Henning Buhl <henning.buhl@yahoo.de>
2021-05-31 11:14:08 +0200

    Create EntityNotFoundExceptionTest.java

---
+++ src/test/java/com/evatool/common/exception/EntityNotFoundExceptionTest.java
@@ -0,0 +1,19 @@
+package com.evatool.common.exception;
+
+import org.junit.jupiter.api.Test;
+
+public class EntityNotFoundExceptionTest {
+
+    @Test
+    void testEntityNotFoundException() {
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+}
