4ca29a7c763589b24b0d147e6efdad56b271bedc
Henning Buhl <henning.buhl@yahoo.de>
2021-05-31 23:12:04 +0200

    Added @Column annotation

--- src/main/java/com/evatool/domain/entity/User.java
+++ src/main/java/com/evatool/domain/entity/User.java
@@ -6,6 +6,7 @@ import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -18,6 +19,7 @@ public class User extends SuperEntity {
     private static final Logger logger = LoggerFactory.getLogger(User.class);
 
     @Getter
+    @Column(name = "external_user_id", nullable = false)
     private String externalUserId;
 
     public User(String externalUserId) {
