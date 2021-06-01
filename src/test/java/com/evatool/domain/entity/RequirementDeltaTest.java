35b080813d6a2fdd42f5096c25d67016b4704286
Henning Buhl <henning.buhl@yahoo.de>
2021-06-01 12:06:03 +0200

    Added delta color test setup

--- src/test/java/com/evatool/domain/entity/RequirementDeltaTest.java
+++ src/test/java/com/evatool/domain/entity/RequirementDeltaTest.java
@@ -1,8 +1,13 @@
 package com.evatool.domain.entity;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DataJpaTest
@@ -11,12 +16,27 @@ class RequirementDeltaTest extends SuperEntityTest {
     @Test
     void testSetDelta_ExceedsBounds_Throw() {
         // given
-        var entity = getPersistedRequirementDelta();
+        var delta = getPersistedRequirementDelta();
 
         // when
 
         // then
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> entity.setOverwriteMerit(-3f));
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> entity.setOverwriteMerit(3f));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> delta.setOverwriteMerit(-3f));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> delta.setOverwriteMerit(3f));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 1, 0, 255, 0"})
+    void testGetMeritColor(Float merit, Float overwriteMerit, int r, int g, int b) {
+        // given
+        var delta = getPersistedRequirementDelta();
+        delta.getImpact().setMerit(merit);
+
+        // when
+        delta.setOverwriteMerit(overwriteMerit);
+
+        // then
+        var color = delta.getMeritColor();
+        assertThat(color).isEqualTo(new Color(r, g, b));
     }
 }
