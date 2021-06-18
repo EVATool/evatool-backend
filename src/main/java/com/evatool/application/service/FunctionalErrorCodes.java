380a90b605834794fde76971979498fd8c371bf3
Henning Buhl <henning.buhl@yahoo.de>
2021-06-18 13:45:21 +0200

    Added two error codes

--- src/main/java/com/evatool/application/service/FunctionalErrorCodes.java
+++ src/main/java/com/evatool/application/service/FunctionalErrorCodes.java
@@ -7,5 +7,7 @@ public class FunctionalErrorCodes {
 
     public static final int VALUE_REFERENCED_BY_IMPACT = 1001;
     public static final int VARIANT_REFERENCED_BY_REQUIREMENT = 1002;
+    public static final int STAKEHOLDER_REFERENCED_BY_IMPACT = 1003;
+    public static final int IMPACT_REFERENCED_BY_REQUIREMENT = 1004;
 
 }
