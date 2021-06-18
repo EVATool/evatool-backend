9f9a197ba6fd0e901decdb21dd9b9205af3b0f7e
Henning Buhl <henning.buhl@yahoo.de>
2021-06-18 14:53:21 +0200

    Renamed Util

---
+++ src/main/java/com/evatool/common/util/Util.java
@@ -0,0 +1,32 @@
+package com.evatool.common.util;
+
+import com.evatool.domain.entity.SuperEntity;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Util {
+
+    public static int iterableSize(Iterable iterable) {
+        int counter = 0;
+        for (Object i : iterable) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public static <O extends SuperEntity> UUID[] entitySetToIdArray(Iterable<O> entitySet) { // Tests
+        var ids = new ArrayList<UUID>();
+        var index = 0;
+        for (var o : entitySet) {
+            ids.add(o.getId());
+            index++;
+        }
+        var idArray = new UUID[index];
+        index = 0;
+        for (var id : ids) {
+            idArray[index] = id;
+        }
+        return idArray;
+    }
+}
