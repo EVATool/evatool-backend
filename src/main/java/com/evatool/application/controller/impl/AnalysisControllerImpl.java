6f007a745fed23af4ccde2b2e4192d0e471dea3a
Henning Buhl <henning.buhl@yahoo.de>
2021-06-28 20:11:11 +0200

    Update AnalysisControllerImpl.java

--- src/main/java/com/evatool/application/controller/impl/AnalysisControllerImpl.java
+++ src/main/java/com/evatool/application/controller/impl/AnalysisControllerImpl.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
@@ -33,6 +34,7 @@ public class AnalysisControllerImpl extends CrudControllerImpl<Analysis, Analysi
     @Override
     @RolesAllowed({AuthUtil.ADMIN_ROLE})
     @GetMapping(UriUtil.ANALYSES)
+    @PreAuthorize("hasAuthority('" + AuthUtil.ADMIN_ROLE + "')")
     public ResponseEntity<Iterable<EntityModel<AnalysisDto>>> findAll() {
         var dtoListFound = service.findAll();
         return new ResponseEntity<>(withLinks(dtoListFound), HttpStatus.OK);
