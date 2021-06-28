4bf6ee84ca903f8737221a0897e71be3c7a99b6c
Henning Buhl <henning.buhl@yahoo.de>
2021-06-28 22:32:37 +0200

    format

--- src/main/java/com/evatool/common/config/MethodSecurityConfig.java
+++ src/main/java/com/evatool/common/config/MethodSecurityConfig.java
@@ -1,12 +1,9 @@
 package com.evatool.common.config;
 
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.method.MethodSecurityMetadataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
