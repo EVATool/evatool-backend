cecdc9956b366908fd59a504b699b75609762c26
Henning Buhl <henning.buhl@yahoo.de>
2021-06-30 20:30:49 +0200

    clean up

--- src/main/java/com/evatool/application/controller/impl/AnalysisControllerImpl.java
+++ src/main/java/com/evatool/application/controller/impl/AnalysisControllerImpl.java
@@ -7,10 +7,6 @@ import com.evatool.common.util.AuthUtil;
 import com.evatool.common.util.UriUtil;
 import com.evatool.domain.entity.Analysis;
 import io.swagger.annotations.Api;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.EntityModel;
@@ -18,11 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 @Api(tags = "Analysis API-Endpoint")
