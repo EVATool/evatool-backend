1637735ccd19d54f28097cc6adedb0b39e92fdb0
Henning Buhl <henning.buhl@yahoo.de>
2021-07-01 13:53:26 +0200

    Added interceptor

---
+++ src/main/java/com/evatool/application/interceptor/AuthInterceptor.java
@@ -0,0 +1,27 @@
+package com.evatool.application.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("PRE HANDLE");
+        request.setAttribute("realm", "evatool-realm");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("POST");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+        System.out.println("AFTER");
+    }
+}
