73d79d1cf3ca4ad69401a54550a55cac2352e2d6
Henning Buhl <henning.buhl@yahoo.de>
2021-07-01 10:17:28 +0200

    Create MultiTenancyConfig.java

---
+++ src/main/java/com/evatool/common/config/MultiTenancyConfig.java
@@ -0,0 +1,14 @@
+package com.evatool.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class MultiTenancyConfig {
+
+    @Value("${keycloak.resource}")
+    private String lel;
+
+
+
+
+
+}
