ea78f079f659928c5b018d40e985e8d47fa24948
Henning Buhl <henning.buhl@yahoo.de>
2021-07-01 15:17:50 +0200

    Update AuthInterceptor.java

--- src/main/java/com/evatool/application/interceptor/AuthInterceptor.java
+++ src/main/java/com/evatool/application/interceptor/AuthInterceptor.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthInterceptor implements HandlerInterceptor {
     // TODO The preHandle method get executed AFTER many auth requests have been processed.
     //  It should fire BEFORE they are processed in order to inject the realm name into the headers.
+    //  This is currently not used but might be if the Realm header is removed and the realm is figured out automatically.
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
