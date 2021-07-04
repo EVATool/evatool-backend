931d23ea7b2bccc7c649996b235c8328271c14e3
Henning Buhl <henning.buhl@yahoo.de>
2021-07-05 01:19:46 +0200

    flyway unable to connect to database

--- src/main/java/com/evatool/common/util/UriUtil.java
+++ src/main/java/com/evatool/common/util/UriUtil.java
@@ -3,6 +3,8 @@ package com.evatool.common.util;
 public class UriUtil {
 
     private UriUtil() {
+        // TODO move keycloak folder to resources folder.
+
         // TODO what happens when requirement references impact with a requirementDelta and the merit of that impact changes (overwrite merit is invalid -> reset? do not allow?) [and more complex cases]
         // TODO Java Generic handling is problematic (code duplication)
         // TODO Only allow entities to have the same analysis (delta.impact.analysis == delta.req.analysis)
