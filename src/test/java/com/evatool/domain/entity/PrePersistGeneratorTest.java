3b44a69781393e3b23d48a30ab852c8e494606a2
Henning Buhl <henning.buhl@yahoo.de>
2021-08-09 16:21:42 +0200

    Moved TODOs into jira

--- src/test/java/com/evatool/domain/entity/PrePersistGeneratorTest.java
+++ src/test/java/com/evatool/domain/entity/PrePersistGeneratorTest.java
@@ -1,6 +1,6 @@
 package com.evatool.domain.entity;
 
-public class PrePersistGeneratorTest { // TODO when class inheritancec in tests is reworked
+public class PrePersistGeneratorTest { // TODO when class inheritance in tests is reworked
 
 
 
