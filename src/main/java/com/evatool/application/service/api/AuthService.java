28f80ddd7974de705bc9ecfaa00725230e8ee96a
Henning Buhl <henning.buhl@yahoo.de>
2021-08-17 15:07:26 +0200

    Added AuthService api

--- src/main/java/com/evatool/application/service/api/AuthService.java
+++ src/main/java/com/evatool/application/service/api/AuthService.java
@@ -1,4 +1,17 @@
 package com.evatool.application.service.api;
 
+import com.evatool.application.dto.AuthRegisterRealmDto;
+import com.evatool.application.dto.AuthRegisterUserDto;
+import com.evatool.application.dto.AuthTokenDto;
+
 public interface AuthService {
+
+    AuthTokenDto login(String username, String password, String realm);
+
+    AuthTokenDto refreshLogin(String refreshToken, String realm);
+
+    AuthRegisterUserDto registerUser(String username, String email, String password);
+
+    AuthRegisterRealmDto registerRealm(String authAdminUsername, String authAdminPassword, String realm);
+
 }
--- src/main/java/com/evatool/application/service/impl/AuthServiceImpl.java
+++ src/main/java/com/evatool/application/service/impl/AuthServiceImpl.java
@@ -1,4 +1,35 @@
 package com.evatool.application.service.impl;
 
-public class AuthServiceImpl {
+import com.evatool.application.dto.AuthRegisterRealmDto;
+import com.evatool.application.dto.AuthRegisterUserDto;
+import com.evatool.application.dto.AuthTokenDto;
+import com.evatool.application.service.api.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class AuthServiceImpl implements AuthService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    @Override
+    public AuthTokenDto login(String username, String password, String realm) {
+        var rest = new RestTemplate();
+        return null;
+    }
+
+    @Override
+    public AuthTokenDto refreshLogin(String refreshToken, String realm) {
+        return null;
+    }
+
+    @Override
+    public AuthRegisterUserDto registerUser(String username, String email, String password) {
+        return null;
+    }
+
+    @Override
+    public AuthRegisterRealmDto registerRealm(String authAdminUsername, String authAdminPassword, String realm) {
+        return null;
+    }
 }
