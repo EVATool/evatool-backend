a7aceea19c72b03ae6df6992699130d616217550
Henning Buhl <henning.buhl@yahoo.de>
2021-08-17 14:52:28 +0200

    Added rest call methods and revamped rest call URIs

--- src/main/java/com/evatool/application/controller/impl/AuthControllerImpl.java
+++ src/main/java/com/evatool/application/controller/impl/AuthControllerImpl.java
@@ -1,4 +1,53 @@
 package com.evatool.application.controller.impl;
 
-public class AuthControllerImpl {
+import com.evatool.application.controller.api.AuthController;
+import com.evatool.application.dto.AuthRegisterRealmDto;
+import com.evatool.application.dto.AuthRegisterUserDto;
+import com.evatool.application.dto.AuthTokenDto;
+import com.evatool.application.service.impl.AuthServiceImpl;
+import com.evatool.common.util.UriUtil;
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Auth API-Endpoint")
+@RestController
+@CrossOrigin
+public class AuthControllerImpl implements AuthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthControllerImpl.class);
+
+    private final AuthServiceImpl authService;
+
+    public AuthControllerImpl(AuthServiceImpl authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    @GetMapping(UriUtil.AUTH_LOGIN)
+    public ResponseEntity<AuthTokenDto> login(String username, String password, String realm) {
+        return null;
+    }
+
+    @Override
+    @GetMapping(UriUtil.AUTH_REFRESH_LOGIN)
+    public ResponseEntity<AuthTokenDto> refreshLogin(String refreshToken, String realm) {
+        return null;
+    }
+
+    @Override
+    @GetMapping(UriUtil.AUTH_REGISTER_USER)
+    public ResponseEntity<AuthRegisterUserDto> registerUser(String username, String email, String password) {
+        return null;
+    }
+
+    @Override
+    @GetMapping(UriUtil.AUTH_REGISTER_REALM)
+    public ResponseEntity<AuthRegisterRealmDto> registerRealm(String authAdminUsername, String authAdminPassword, String realm) {
+        return null;
+    }
 }
--- src/main/java/com/evatool/common/util/UriUtil.java
+++ src/main/java/com/evatool/common/util/UriUtil.java
@@ -53,10 +53,10 @@ public class UriUtil {
     public static final String VARIANTS_REL = "variants";
 
     // Auth.
-    public static final String LOGIN = "/auth/login";
-    public static final String REFRESH = "/auth/refresh";
-    public static final String REGISTER_USER = "/auth/register/user";
-    public static final String REGISTER_REALM = "/auth/register/realm";
+    public static final String AUTH_LOGIN = "/auth/login";
+    public static final String AUTH_REFRESH_LOGIN = "/auth/refresh";
+    public static final String AUTH_REGISTER_USER = "/auth/register/user";
+    public static final String AUTH_REGISTER_REALM = "/auth/register/realm";
 
     // Dynamic keycloak urls. Move this to service impl?
     private static boolean keycloakAuthBaseUrl;
