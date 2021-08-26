d2d3b673143aa004e84654d8df9b519bfa5f5b08
Henning Buhl <henning.buhl@yahoo.de>
2021-08-26 23:40:05 +0200

    Added EmailValidation and tests

---
+++ src/main/java/com/evatool/common/validation/EmailValidation.java
@@ -0,0 +1,21 @@
+package com.evatool.common.validation;
+
+import java.util.regex.Pattern;
+
+public class EmailValidation {
+
+    private EmailValidation() {
+    }
+
+    private static final Pattern EMAIL_REGEX_PATTERN =
+            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+
+    public static String validateEmail(String email) {
+        if (email == null || email.equals("")) {
+            return null; // Do not generally disallow a null or empty email.
+        } else if (!EMAIL_REGEX_PATTERN.matcher(email).matches()) {
+            return "Email \"" + email + "\" is invalid";
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/evatool/common/validation/EmailValidationTest.java
@@ -0,0 +1,35 @@
+package com.evatool.common.validation;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.evatool.common.validation.EmailValidation.validateEmail;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmailValidationTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "null", "a@b", "3tz784.q34dcnz7899w3c45hn78.d34rt9bgn67@8oq3c4nz57"})
+    void testValidateEmail_ValidEmail_OK(String email) {
+        // given
+        email = email.equals("null") ? null : email;
+
+        // when
+        var error = validateEmail(email);
+
+        // then
+        assertThat(error).isNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a@b@c", " ", "%6", "a.ö@gmail.com"})
+    void testValidateEmail_InvalidEmail_Error(String email) {
+        // given
+
+        // when
+        var error = validateEmail(email);
+
+        // then
+        assertThat(error).isNotNull();
+    }
+}
