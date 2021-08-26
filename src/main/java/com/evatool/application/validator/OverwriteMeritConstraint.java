8f0d9f2e5ae7a6c7ae593de92b7938d8e0477d86
Henning Buhl <henning.buhl@yahoo.de>
2021-08-27 00:16:46 +0200

    Moved Validator into Constraint

--- src/main/java/com/evatool/application/validator/OverwriteMeritConstraint.java
+++ src/main/java/com/evatool/application/validator/OverwriteMeritConstraint.java
@@ -1,11 +1,17 @@
 package com.evatool.application.validator;
 
+import com.evatool.application.dto.RequirementDeltaDto;
+
 import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
+import static com.evatool.common.validation.OverwriteMeritValidation.validateOverwriteMerit;
+
 @Documented
-@Constraint(validatedBy = OverwriteMeritValidator.class)
+@Constraint(validatedBy = OverwriteMeritConstraint.OverwriteMeritValidator.class)
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OverwriteMeritConstraint {
@@ -14,4 +20,24 @@ public @interface OverwriteMeritConstraint {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
+    class OverwriteMeritValidator implements ConstraintValidator<OverwriteMeritConstraint, RequirementDeltaDto> {
+
+        @Override
+        public boolean isValid(RequirementDeltaDto requirementDeltaDto, ConstraintValidatorContext constraintValidatorContext) {
+            if (requirementDeltaDto.getOriginalMerit() == null || requirementDeltaDto.getOverwriteMerit() == null) {
+                return true;
+            }
+
+            var error = validateOverwriteMerit(requirementDeltaDto.getOverwriteMerit(), requirementDeltaDto.getOriginalMerit());
+            if (error != null) {
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext.buildConstraintViolationWithTemplate(error).addConstraintViolation();
+                return false;
+            }
+
+            return true;
+        }
+    }
+
 }
