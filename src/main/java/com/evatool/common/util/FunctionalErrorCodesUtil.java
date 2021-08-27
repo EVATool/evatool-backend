ed17a55cab2e6be0e6afcd3c7f4bfe5b792c1963
Henning Buhl <henning.buhl@yahoo.de>
2021-08-27 17:21:58 +0200

    Added conflict functional error codes for auth

--- src/main/java/com/evatool/common/util/FunctionalErrorCodesUtil.java
+++ src/main/java/com/evatool/common/util/FunctionalErrorCodesUtil.java
@@ -10,6 +10,8 @@ public class FunctionalErrorCodesUtil {
     public static final int VARIANT_REFERENCED_BY_REQUIREMENT = 1002;
     public static final int STAKEHOLDER_REFERENCED_BY_IMPACT = 1003;
     public static final int IMPACT_REFERENCED_BY_REQUIREMENT_DELTA = 1004;
+    public static final int REGISTER_USERNAME_ALREADY_EXISTS = 1051;
+    public static final int REGISTER_EMAIL_ALREADY_EXISTS = 1052;
 
     // 404 Not found codes.
     public static final int LOGIN_REALM_NOT_FOUND = 2001;
