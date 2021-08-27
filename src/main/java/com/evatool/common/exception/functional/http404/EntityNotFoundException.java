6a42dcce4312d3ec53ef2052dcd6c88fba968978
Henning Buhl <henning.buhl@yahoo.de>
2021-08-27 18:34:50 +0200

    Made EntityNotFoundException functional

--- src/main/java/com/evatool/common/exception/functional/http404/EntityNotFoundException.java
+++ src/main/java/com/evatool/common/exception/functional/http404/EntityNotFoundException.java
@@ -1,17 +1,22 @@
 package com.evatool.common.exception.functional.http404;
 
+import com.evatool.common.exception.functional.Tag;
+import com.evatool.common.util.FunctionalErrorCodesUtil;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.UUID;
 
-public class EntityNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends NotFoundException {
     public EntityNotFoundException(String entityClass, UUID id) {
-        this(entityClass, id, "id");
+        super(String.format("Entity '%s' with id '%s' not found", entityClass, id), FunctionalErrorCodesUtil.ENTITY_NOT_FOUND, new EntityNotFoundTag(entityClass, id));
     }
 
-    public EntityNotFoundException(String entityClass, UUID id, String field) {
-        this(entityClass, id == null ? "null" : id.toString(), field);
-    }
-
-    public EntityNotFoundException(String entityClass, String id, String field) {
-        super(String.format("The entity of type '%s' with %s '%s' was not found", entityClass, field, id));
+    @Getter
+    @RequiredArgsConstructor
+    public static class EntityNotFoundTag extends Tag {
+        private final String entityClass;
+        private final UUID id;
+        private final int entityCode = 0; // TODO
     }
 }
