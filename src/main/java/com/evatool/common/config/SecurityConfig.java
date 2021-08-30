b30182ae703546d7231340af14c7c719c7c36808
Henning Buhl <henning.buhl@yahoo.de>
2021-08-30 13:33:28 +0200

    Functional errors work again but csrf is ignoring all requests

--- src/main/java/com/evatool/common/config/SecurityConfig.java
+++ src/main/java/com/evatool/common/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -51,16 +52,14 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         super.configure(http);
         if (authEnabled) { // This statement cannot be re-written with an in-between if-statement.
             http
-                    //.csrf().disable()
-                    .csrf().ignoringAntMatchers("/auth/**").csrfTokenRepository(this.getCsrfTokenRepository()).and()
+                    .csrf().ignoringAntMatchers("/**").csrfTokenRepository(this.getCsrfTokenRepository()).and()
                     .cors().and()
                     .authorizeRequests()
                     .anyRequest()
                     .permitAll();
         } else {
             http
-                    //.csrf().disable()
-                    .csrf().ignoringAntMatchers("/auth/**").csrfTokenRepository(this.getCsrfTokenRepository()).and()
+                    .csrf().disable()
                     .cors().and()
                     .addFilterAfter(new SkipSecurityFilter(), CsrfFilter.class)
                     .authorizeRequests()
