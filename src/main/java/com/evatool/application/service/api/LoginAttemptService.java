98593a52b7cf484055c11af1c0adff2351c9d3c4
Henning Buhl <henning.buhl@yahoo.de>
2021-09-07 16:01:30 +0200

    Removed redundant signatures

--- src/main/java/com/evatool/application/service/api/LoginAttemptService.java
+++ src/main/java/com/evatool/application/service/api/LoginAttemptService.java
@@ -6,10 +6,10 @@ public interface LoginAttemptService {
 
     void loginSucceeded(String key);
 
-    public void loginFailed(String key);
+     void loginFailed(String key);
 
-    public boolean isBlocked(String key);
+    boolean isBlocked(String key);
 
-    public int getRemainingAttempts(String key);
+    int getRemainingAttempts(String key);
 
 }
