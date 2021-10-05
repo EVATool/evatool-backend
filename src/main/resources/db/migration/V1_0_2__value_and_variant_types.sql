df4dce7031d55b3178f25cf5cfd384001e138417
Henning Buhl <henning.buhl@yahoo.de>
2021-10-05 18:38:45 +0200

    Update V1_0_2__value_and_variant_types.sql

--- src/main/resources/db/migration/V1_0_2__value_and_variant_types.sql
+++ src/main/resources/db/migration/V1_0_2__value_and_variant_types.sql
@@ -30,3 +30,51 @@ alter table variant_type
 
 
 
+-- ###############
+-- Add relations.
+-- ###############
+
+-- Add ValueTypeId to Value.
+alter table value
+    add value_type_id CHAR(36) null;
+
+-- Add VariantTypeId to Variant.
+alter table variant
+    add variant_type_id CHAR(36) null;
+
+
+
+
+-- ###############
+-- Enforce foreign key relations.
+-- ###############
+
+-- Make value_type_id not null.
+alter table value
+    alter column value_type_id CHAR(36) not null;
+
+-- Add foreign key constraint to value.
+alter table value
+    add constraint FK_value__value_type_id__value_type__id
+    foreign key (value_type_id)
+    references value_type (id);
+
+-- Make variant_type_id not null.
+alter table variant
+    alter column variant_type_id CHAR(36) not null;
+
+-- Add foreign key constraint to variant.
+alter table variant
+    add constraint FK_variant__variant_type_id__variant_type__id
+    foreign key (variant_type_id)
+    references variant_type (id);
+
+
+
+-- ###############
+-- Remove deprecated columns.
+-- ###############
+
+-- Remove type column from value table.
+alter table value
+    drop column type;
